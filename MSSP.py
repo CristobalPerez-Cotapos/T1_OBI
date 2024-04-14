64779efb0bf63a6ad4cd57cc98e14375a0859e01
CristobalPerez-Cotapos <cprezcotapos@uc.cl>
2024-04-13 20:37:55 -0400

    funciona la raja

--- MSSP.py
+++ MSSP.py
@@ -1,4 +1,48 @@
-import gurobipy as gp 
+import numpy as np
+import matplotlib.pyplot as plt
+import gurobipy as gp
+
+
+
+def graficar_region_factible(cortes, iter, sol, L=0, x_vals=np.linspace(-1, 4, 100)):
+
+    # Tamaño de la figura
+    plt.figure(figsize=(10, 6))
+    ax = plt.gca()  # Get current axes
+    ax.set_facecolor('black')
+
+    # Draw x and y axis lines
+    plt.axhline(0, color='white', lw=1)  # Add y-axis through the origin
+    plt.axvline(0, color='white', lw=1)  # Add x-axis through the origin
+    
+    # Hacer un llenado inical
+    plt.plot(x_vals, [L for _ in x_vals], color='red', linestyle='--',label=f'Cota inferior')
+    plt.fill_between([0, 3], 0, 10, color='grey', alpha=0.4, label='Region Factible')
+    
+    # Para cada corte, llenar el area infactible
+    for corte in cortes:
+
+        coeffs = corte[0]
+
+        if corte[-1] == 'opt':
+            y_vals = [coeffs[1] * x_val + coeffs[0] for x_val in x_vals]
+            plt.plot(x_vals, y_vals, label=f'Corte de opt {cortes.index(corte)+1}')
+            plt.fill_between(x_vals, -10, y_vals, color='black', alpha=0.5)
+        else:
+            const = -coeffs[0] / coeffs[1]
+            plt.axvline(x=const, color='blue', linestyle='--', label=f'Corte de fact {cortes.index(corte)+1}')
+            plt.fill_betweenx(np.linspace(-10, 10, 100), 0, const, color='black', alpha=0.5)
+
+    # Plot candidate point
+    plt.scatter(sol[0], sol[1], color='red', zorder=5, label='Solucion candidata')
+    
+    plt.xlim(x_vals[0], x_vals[-1])
+    plt.ylim(-1, 10)
+    plt.xlabel('x')
+    plt.ylabel('theta')
+    plt.title(f'Iteration {iter}')
+    plt.legend()
+    plt.show()
 
 
 class Problema:
@@ -7,6 +51,8 @@ class Problema:
         self.padre = None
         self.nodo = nodo
         self.h = h
+        self.terminado = False
+        self.cortes = []
 
     def agregar_hijo(self, hijo):
         self.hijos.append(hijo)
@@ -52,6 +98,7 @@ class Problema:
                 self.agregar_corte(self.calcular_coeficinetes())
             else:
                 print("No se agrega corte")
+                self.terminado = True
             self.problema.update()
 
             self.problema.optimize()
@@ -60,31 +107,35 @@ class Problema:
 
     @property
     def variables_duales(self):
-
-        if self.nodo == (2,1):
-            for i in self.problema.getConstrs():
-                print(i.Pi)
-
         return (self.problema.getConstrByName("c1").Pi, self.problema.getConstrByName("NV").Pi)
 
     
 
 
     def calcular_coeficinetes(self):
-        print("Variables duales:")
-        print(self.hijos[0].variables_duales)
-        print(self.hijos[1].variables_duales)
-        coeffs = (sum((1/2) * (hijo.h * hijo.variables_duales[0]) for hijo in self.hijos), -sum((1/2) * hijo.variables_duales[0] for hijo in self.hijos))
-        return coeffs
+        suma_positiva = 0
+        suma_negativa = 0
+        for hijo in self.hijos:
+            suma_positiva += (1/2) * hijo.h * hijo.problema.getConstrByName("c1").getAttr("Pi")
+            suma_negativa += (1/2) * hijo.problema.getConstrByName("c1").getAttr("Pi")
+
+            # Tomo todas las otras restricciones que no son la c1
+            restricciones = filter(lambda x: x.getAttr("ConstrName") != "c1", hijo.problema.getConstrs())
+
+            for restriccion in restricciones:
+                suma_positiva += restriccion.getAttr("rhs") * restriccion.getAttr("Pi") * (1/2)
+
+        return (suma_positiva, -suma_negativa)
+
+
     
     def agregar_corte(self, coeffs):
         self.problema.addConstr(coeffs[0] + coeffs[1] * self.x <= self.tetha, name="corte")
         print(f"************Corte de optimalidad en {self.nodo}: {coeffs[0]} + {coeffs[1]}x <= theta*************")
         #self.problema.addConstr(gp.quicksum(((1/2) * (hijo.h - self.x) * hijo.variables_duales[0]) for hijo in self.hijos) <= self.tetha, name="corte")
-
-
-
         self.problema.update()
+        self.cortes.append((coeffs, "opt"))
+
 
     def imprimir_resultado(self):
         print(f"X{self.nodo} = {self.x.X}", f"tetha{self.nodo} = {self.tetha.X}")
@@ -117,14 +168,16 @@ if __name__ == "__main__":
         for nieto in hijo.hijos:
             nieto.crear_modelo()
 
-
-    for i in range(3):
+    iteracion = 0
+    while not raiz.terminado:
         raiz.iteracion()    
         print("\n" * 2)
-        print(f"XXXXXXXXX Solucion iteracion {i} XXXXXXXXX")  
+        print(f"XXXXXXXXX Solucion iteracion {iteracion} XXXXXXXXX")
         print(raiz.problema.objVal)
         raiz.imprimir_resultado()
         print("\n" * 2)
+        iteracion += 1
+        graficar_region_factible(raiz.cortes, iteracion, (raiz.x.X, raiz.tetha.X))
 
 
 
